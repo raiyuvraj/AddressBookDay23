a11014fe63dc8829e59161296d804041430fd112
raiyuvraj <yuvirai017@gmail.com>
2022-12-27 17:20:33 -0800

    [Yuvraj]-Create Contact

---
+++ AddressBookDay23/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDay23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program:");
+            AddressBook ab = new AddressBook("Yuvraj", "Rai", "3006", "Noida", "UP", "uv@gmail.com", 201016, 8218501015);
+            ab.DisplayContacts();
+
+        }
+    }
+}
